e0c91608d16eeec60d2dc9b1ef840e852da73bad
niitish <71326553+niitish@users.noreply.github.com>
2022-12-26 00:38:36 +0530

    add PredictionHelper and saved models

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 from uuid import uuid4
+from utils.PredictionHelper import PredictionHelper
+
 
 app = Flask(__name__, static_folder="public",
             static_url_path="/public", template_folder="views")
 
 app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "uploads")
-app.config["MAX_CONTENT_LENGTH"] = 2 * 1024 * 1024
+app.config["MAX_CONTENT_LENGTH"] = 4 * 1024 * 1024
+app.config["TESTING"] = False
 
 ALLOWED_EXTENSIONS = ["image/png", "image/jpg", "image/jpeg"]
 
@@ -39,6 +42,15 @@ def get_pred():
         return "Only PNG and JPG/JPEG files are allowed."
     else:
         fileExtension = f.mimetype.split("/")[1]
-        f.save(os.path.join(
-            app.config['UPLOAD_FOLDER'], secure_filename(uuid4().hex+"."+fileExtension)))
-        return render_template('pred.html')
+        fileName = secure_filename(uuid4().hex+"."+fileExtension)
+        filePath = os.path.join(app.config['UPLOAD_FOLDER'], fileName)
+        f.save(filePath)
+
+        ph = PredictionHelper(filePath)
+        res = ph.run()
+
+        return render_template('pred.html', result=res)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ utils/PredictionHelper.py
@@ -0,0 +1,66 @@
+
+import skimage.feature as skf
+import pandas as pd
+import numpy as np
+import cv2 as cv
+import sklearn  # not required, just to repress the import error
+import joblib
+HEIGHT = 392
+WIDTH = 392
+DISTANCE = 10
+THETA = 90
+
+
+class PredictionHelper:
+    def __init__(self, path):
+        self.path_to_image = path
+        self.coocurrence_matrix = None
+        self.features = np.zeros(5)
+        self.scaled_features = None
+
+    def glcm_feature(self, feature_name):
+        feature = skf.graycoprops(self.coocurrence_matrix, feature_name)
+        result = np.average(feature)
+        return result
+
+    def preprocess(self):
+        img = cv.imread(self.path_to_image)
+        gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        thresh_img = cv.adaptiveThreshold(
+            gray_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 3)
+
+        cnts = cv.findContours(
+            thresh_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        cnts = sorted(cnts, key=cv.contourArea, reverse=True)
+        for c in cnts:
+            x, y, w, h = cv.boundingRect(c)
+            thresh_img = thresh_img[y:y+h, x:x+w]
+            break
+
+        img = cv.resize(thresh_img, (WIDTH, HEIGHT))
+
+        self.coocurrence_matrix = skf.graycomatrix(img, [DISTANCE], [THETA], 256,
+                                                   symmetric=True, normed=True)
+        self.features[0] = self.glcm_feature('contrast')
+        self.features[1] = self.glcm_feature('homogeneity')
+        self.features[2] = self.glcm_feature('energy')
+        self.features[3] = self.glcm_feature('correlation')
+        self.features[4] = self.glcm_feature('dissimilarity')
+
+    def scale_data(self):
+        scaler = joblib.load('scaler.joblib')
+        data = pd.DataFrame([self.features], columns=[
+                            'contrast', 'homogeneity', 'energy', 'correlation', 'dissimilarity'])
+        self.scaled_features = scaler.transform(data)
+
+    def predict(self):
+        model = joblib.load('model.joblib')
+        result = model.predict_proba(self.scaled_features)
+        return result
+
+    def run(self):
+        self.preprocess()
+        self.scale_data()
+        result = self.predict()
+        return round(result[0][1]*100, 3)
